a5f76defa5c624afbc73528b643e1bf030533a39
Maria Eichman <m.levanuck@gmail.com>
2018-03-31 17:17:11 -0500

    Add project files.

---
+++ Twisted wheel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Twisted_wheel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
